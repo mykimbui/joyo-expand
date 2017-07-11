ae176a2d7a4038b48eb948d958d06bee83ad7606
My Kim Bui <mykimbuibui@gmail.com>
2017-07-11 17:56:25 +0800

    js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-// define document width and height
-TweenMax.set('.container', {scale: 0.2})
-TweenMax.to('.container', 10 + 5 * Math.random(), {rotation: Math.random()< .5? '+=360' : '-=360', repeat: -1, ease: Linear.easeNone, height: 2000 + Math.random(), yoyo: true}, .25)
+//  var htmlElements = "";
+//  for (var i = 0; i < 10; i++) {
+//   htmlElements += '<div class="container"><div class="line"></div></div>';
+// }
+
+var container = $("#container");
+var elements = []
+var items;
+for (var i = 6; i >= 0; i--) {
+  item = $('<div class="container"><div class="line"></div></div>')
+  elements.push(item)
+  container.append(item)
+}
+
+elements.forEach(function (item, i){
+  TweenMax.set(item, {left: window.innerWidth * Math.random(), top: window.innerHeight * Math.random(), width: 300})
+  TweenMax.to(item, 10 + 5 * Math.random(), {x: Math.random(), y: Math.random(), rotation: Math.random()< .5? '+=360' : '-=360', repeat: -1, ease: Linear.easeNone, height: 180 + Math.random(), yoyo: true, force3D:false}, .25)
+})
 });
